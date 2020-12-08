2fbd6f6fccd5a40464676393fe3d9ec8089ad31a
voletiananth <voletiananthkumar@gmail.com>
2020-12-08 22:45:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("addEmplyoeeForm").style.display = "block";
+    console.log("open")
+}
+
+function closeForm() {
+    document.getElementById("addEmplyoeeForm").style.display = "none";
+}
