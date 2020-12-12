0fa1c3d0f4120fe1a128a57cb0ead0b132be8b5c
voletiananth <voletiananthkumar@gmail.com>
2020-12-12 23:26:50 +0530

    validation

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function openForm() {
+function openForm(){
     document.getElementById("addEmployeeForm").style.display = "block";
     
 }
 
 function closeForm() {
-    document.getElementById("addEmployeeForm").style.display = "none";
+  
+    document.getElementById("addEmployeeForm").style.display = "none"
+    const temp =   document.getElementsByClassName("error")
+    Array.from(temp).forEach((e)=>{
+      e.innerHTML=""
+    })
 }
 
 
@@ -25,7 +30,7 @@ class Employee {
 
 var employeeData = [];
 
-var formInputs = ["fname","lname","email","job_title","office","department","phoneNumber","skypeId"];
+var formInputs = ["fname","lname","email","job_title","office","department","phone_number","skype_id"];
 
 function submitEmployee(){
    
@@ -82,4 +87,103 @@ function addEmployee(employee) {
 }
 
 
-const form = document.getElementById("emplyoee_form")
+
+
+
+
+const regValidate = [
+  {id:"fname",reg:"^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$",error_id:"fname_error",error_msg:"Please enter your first name"}
+  ,{id:"lname",reg:"^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$",error_id:"lname_error",error_msg:"please enter your last name"},
+  {id:"email",reg:"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" ,error_id:"email_error",error_msg:"please enter a valid email address"},
+  {id:"phone_number",reg:"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$",error_id:"phone_number_error",error_msg:"please enter a valid phone number"},  
+  
+]
+
+const selectValidate =[
+  {id:"job_title",select_id:"job_title_select",error_msg:"please prefer a job title",error_id:"job_title_error"},
+  {id:"office",select_id:"office_select",error_msg:"please prefer an office",error_id:"office_error"},
+  {id:"department",select_id:"department_select",error_msg:"please prefer a department",error_id:"department_error"},
+]
+
+
+regValidate.forEach((e)=>{
+  const ele =document.getElementById(e.id)
+  ele.addEventListener("focus",(_)=>{
+    document.getElementById(e.error_id).innerHTML =""
+  })
+})
+
+selectValidate.forEach((e)=>{
+  const ele =document.getElementById(e.id)
+  ele.addEventListener("focus",(_)=>{
+    document.getElementById(e.error_id).innerHTML =""
+  })
+
+})
+
+document.getElementById("skype_id").addEventListener("focus",(_)=>{
+  document.getElementById("skype_id_error").innerHTML =""
+})
+
+
+
+var firstName = ""
+var lastName = ""
+
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+
+
+fname.addEventListener("input",(_)=>{
+  firstName = fname.value
+  console.log(firstName)
+  document.getElementById('pname').value = firstName+" "+lastName
+})
+
+lname.addEventListener("input",(_)=>{
+  lastName = lname.value
+  document.getElementById('pname').value = firstName+" "+lastName
+})
+
+
+
+function validation(){
+  var flag = true;
+  regValidate.forEach((e)=>{
+    const val = document.getElementById(e.id);
+    const regx = new RegExp(e.reg)
+    if(regx.test(val.value)){
+      document.getElementById(e.error_id).innerHTML = ""
+    }else{
+      document.getElementById(e.error_id).innerHTML = e.error_msg
+      flag = false
+    }
+  })
+
+  selectValidate.forEach((e)=>{
+    const val = document.getElementById(e.id);
+    if(val.value==""){
+      document.getElementById(e.error_id).innerHTML = e.error_msg
+      flag = false
+    }else{
+      document.getElementById(e.error_id).innerHTML =""    }
+  })
+
+  const skypeId = document.getElementById("skype_id")
+  if(skypeId.value==""){
+    flag = false
+    document.getElementById("skype_id_error").innerHTML = "please enter a valid skypeid"
+  }else{
+    document.getElementById("skype_id_error").innerHTML = ""
+  }
+
+  if(flag){
+   const values = formInputs.map((e)=>{
+     const temp = document.getElementById(e)
+      return temp.value;
+    })
+    addEmployee(new Employee(values[0],values[1],values[2],values[3],values[4],values[5],values[6],values[7]))
+    closeForm()
+  }
+
+}
