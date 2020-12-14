fc8e8fd1581f0cbdab1e56735f97f977d0a55ab0
voletiananth <voletiananthkumar@gmail.com>
2020-12-14 10:27:42 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,12 +1,199 @@
 function openForm() {
-  document.getElementById("addEmployeeForm").style.display = "block";
+  var formdiv = document.getElementById("addEmployeeForm")
+  formdiv.innerHTML = ` <form id="employee_form" novalidate>
+ <div>
+  <div class="item a-inline" >
+    <div class="item-label">
+      <p>First Name:</p>
+    </div>
+    <div class="item-input">
+        <input id="fname" type="text">
+        <span id="fname_error" class="error"></span>
+    </div>
+  </div>
+
+  <div   class="item a-inline" >
+    <div class="item-label">
+      <p>Last Name:</p>
+    </div>
+    <div class="item-input">
+        <input id="lname" type="text">
+        <span id="lname_error" class="error"></span>
+    </div>
+  </div>
+ </div>
+
+
+ <div class="item" >
+  <div class="item-label">
+    <p>Preferred Name:</p>
+  </div>
+  <div class="item-input">
+      <input id="pname" type="text" disabled>
+  </div>
+</div>
+
+<div class="item" >
+  <div class="item-label">
+    <p>Email:</p>
+  </div>
+  <div class="item-input">
+      <input id="email" type="email">
+      <span id="email_error" class="error"></span>
+  </div>
+</div>
+
+
+<div class="item">
+  <div>
+    <div class="item-label">
+      <p>Job Title:</p>
+    </div>
+    <div class="flex-div"  >
+       <div class="item-input">
+         <input id="job_title" type="text">
+         <span id="job_title_error" class="error"></span>  
+       </div>
+       <div class="input-middle">--or--</div>
+       <div class="input-last" >
+         <select id="job_title_select" class="width-100"  > 
+          <option>Select</option>
+        </select>
+       </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="item">
+  <div>
+    <div class="item-label">
+      <p>Office:</p>
+    </div>
+    <div class="flex-div"  >
+       <div class="item-input">
+         <input id="office" type="text">
+         <span id="office_error" class="error"></span>  
+       </div>
+       <div class="input-middle">--or--</div>
+       <div class="input-last" >
+         <select id="office_select" class="width-100"  > 
+          <option>Select</option>
+        </select>
+       </div>
+    </div>
+  </div>
+</div>
+
+<div class="item">
+  <div>
+    <div class="item-label">
+      <p>Department:</p>
+    </div>
+    <div class="flex-div"  >
+       <div class="item-input">
+         <input id="department" type="text">
+         <span id="department_error" class="error"></span>  
+       </div>
+       <div class="input-middle">--or--</div>
+       <div class="input-last" >
+         <select id="department_select" class="width-100"  > 
+          <option>Select</option>
+        </select>
+       </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="item" >
+  <div class="item-label">
+    <p>Phone Number:</p>
+  </div>
+  <div class="item-input">
+      <input id="phone_number" type="text">
+      <span id="phone_number_error" class="error"></span>
+  </div>
+</div>
+
+
+<div class="item" >
+  <div class="item-label">
+    <p>Skype ID:</p>
+  </div>
+  <div class="item-input">
+      <input id="skype_id" type="text">
+      <span id="skype_id_error" class="error"></span>
+  </div>
+</div>
+<div style="padding: 16px 8px;">
+  <button type="button" onclick="closeForm()" style="float: left;">Cancel</button>
+  <button type="button" onclick="validation()" style="float: right;">Submit</button>
+</div>
+
+ 
+
+</form>`
+
+  formdiv.style.display = "block";
+
+  addListeners()
 
 }
 
+
+
+
+
 function closeForm() {
 
-  document.getElementById("addEmployeeForm").style.display = "none"
+  var formdiv = document.getElementById("addEmployeeForm")
+  formdiv.style.display = "none"
+  formdiv.innerHTML = ''
 
+
+}
+
+function addListeners() {
+
+  regValidate.forEach((e) => {
+    const ele = document.getElementById(e.id)
+    ele.addEventListener("focus", (_) => {
+      document.getElementById(e.error_id).innerHTML = ""
+    })
+  })
+
+  selectValidate.forEach((e) => {
+    const ele = document.getElementById(e.id)
+    ele.addEventListener("focus", (_) => {
+      document.getElementById(e.error_id).innerHTML = ""
+    })
+
+  })
+
+  document.getElementById("skype_id").addEventListener("focus", (_) => {
+    document.getElementById("skype_id_error").innerHTML = ""
+  })
+
+
+
+  var firstName = ""
+  var lastName = ""
+
+  const fname = document.getElementById('fname')
+  const lname = document.getElementById('lname')
+
+
+  fname.addEventListener("input", (_) => {
+    firstName = fname.value
+    console.log(firstName)
+    document.getElementById('pname').value = firstName + " " + lastName
+  })
+
+  lname.addEventListener("input", (_) => {
+    lastName = lname.value
+    document.getElementById('pname').value = firstName + " " + lastName
+  })
 }
 
 
@@ -70,17 +257,17 @@ var filters = [{
   id: "offices",
   name: "Offices",
   filter: [
-  { name: "India", count: 0 },
-  { name: "Seattle", count: 0 },]
+    { name: "India", count: 0 },
+    { name: "Seattle", count: 0 },]
 }, {
   id: "job_titles",
   name: "Job Titles",
   filter: [
-  { name: "SharePoint Practice Head", count: 0 },
-  { name: ".Net Development Lead", count: 0 },
-  { name: "Recuriting Expert", count: 0 },
-  { name: "BI Developer", count: 0 },
-  { name: "Business Analyst", count: 0 },]
+    { name: "SharePoint Practice Head", count: 0 },
+    { name: ".Net Development Lead", count: 0 },
+    { name: "Recuriting Expert", count: 0 },
+    { name: "BI Developer", count: 0 },
+    { name: "Business Analyst", count: 0 },]
 }]
 
 
@@ -91,63 +278,26 @@ function addFilters() {
   filters.forEach((filterItems) => {
     const div = document.createElement("div")
     div.className = 'department category mid-black'
-    const h4 = "<h4>"+filterItems.name+"</h4>"
+    const h4 = "<h4>" + filterItems.name + "</h4>"
     const _filter = filterItems.filter
-    const li =  _filter.map((ele)=>{
-      return "<li><a>"+ele.name+" ("+ele.count+")</a></li>"
+    const li = _filter.map((ele) => {
+      return "<li><a>" + ele.name + " (" + ele.count + ")</a></li>"
     }).join("")
 
-    const filterData = h4+"<ul>"+li+"</ul>"
+    const filterData = h4 + "<ul>" + li + "</ul>"
 
     div.innerHTML = filterData
 
     document.getElementById("filter-content").appendChild(div)
 
-    
+
 
   })
 }
 
 addFilters()
 
-regValidate.forEach((e) => {
-  const ele = document.getElementById(e.id)
-  ele.addEventListener("focus", (_) => {
-    document.getElementById(e.error_id).innerHTML = ""
-  })
-})
 
-selectValidate.forEach((e) => {
-  const ele = document.getElementById(e.id)
-  ele.addEventListener("focus", (_) => {
-    document.getElementById(e.error_id).innerHTML = ""
-  })
-
-})
-
-document.getElementById("skype_id").addEventListener("focus", (_) => {
-  document.getElementById("skype_id_error").innerHTML = ""
-})
-
-
-
-var firstName = ""
-var lastName = ""
-
-const fname = document.getElementById('fname')
-const lname = document.getElementById('lname')
-
-
-fname.addEventListener("input", (_) => {
-  firstName = fname.value
-  console.log(firstName)
-  document.getElementById('pname').value = firstName + " " + lastName
-})
-
-lname.addEventListener("input", (_) => {
-  lastName = lname.value
-  document.getElementById('pname').value = firstName + " " + lastName
-})
 
 
 
